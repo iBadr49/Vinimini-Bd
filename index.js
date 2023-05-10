bb9a244661493aa77402b6e9ce50d40dad8dcc59
iBadr49 <badr.amara@hva.nl>
2023-05-10 10:14:22 +0200

    code cleaner

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import express from "express";
 import { ppid } from "process";
 
 const url = "https://api.vinimini.fdnd.nl/api/v1/producten"; // URL naar Json data
-const url2 = 'https://api.vinimini.fdnd.nl/api/v1';
+const url2 = "https://api.vinimini.fdnd.nl/api/v1";
 
 // Maak een nieuwe express app aan
 const app = express();
@@ -18,55 +18,25 @@ app.use(express.static("public"));
 
 // Maak een route voor de index
 
-app.get('/', (request, response) => {
-  response.render('index')
-})
+app.get("/", (request, response) => {
+  response.render("index");
+});
 
+app.get("/proces", (request, response) => {
+  let id = request.query.productId || "clerg7e1y032z0auq7ex5rid9";
+  let detailUrl = url2 + "/product?id=" + id;
+
+  fetchJson(detailUrl).then((data) => {
+    response.render("proces", data);
+  });
+});
 
 app.get("/producten", (request, response) => {
-  let productenUrl = url ;
+  let productenUrl = url;
   fetchJson(productenUrl).then((data) => {
     response.render("producten", data);
   });
 });
-
-app.get('/proces', (request, response) => {
-  let id = request.query.productId || "clerg7e1y032z0auq7ex5rid9";
-  let detailUrl = url2 + "/product?id=" + id;
-  
-  fetchJson(detailUrl).then((data) => {
-    response.render("proces", data);
-  });
-})
-
-// Post note (notitie) to API
-app.post('/agenda', function (req, res, next) {
-
-  req.body.afgerond = false
-  //req.body.persoonId = 'clemozv3c3eod0bunahh71sx7'
-  req.body.datum = req.body.datum + ':00Z';
-  req.body.herinnering = [req.body.herinnering + ':00Z']
-  console.log(req.body)
-  
-  postJson(url2 + '/notities', req.body).then((data) => {
-
-    // console.log(JSON.stringify(data))
-
-    let newNotitie = { ... req.body }
-    
-    if (data.success) {
-      res.redirect('/agenda') 
-      // TODO: squad meegeven, message meegeven
-      // TODO: Toast meegeven aan de homepagina
-    } else {
-      const errormessage = `${data.message}: Mogelijk komt dit door de slug die al bestaat.`
-      const newdata = { error: errormessage, values: newNotitie }
-      
-      res.render('agenda', newdata)
-    }
-  })
-});
-
 
 app.get("/detail", (request, response) => {
   let id = request.query.detailId || "clerps05z09jm0aw3vccjq5un";
@@ -78,11 +48,35 @@ app.get("/detail", (request, response) => {
   });
 });
 
+// Post note (notitie) to API
+app.post("/agenda", function (req, res, next) {
+  req.body.afgerond = false;
+  //req.body.persoonId = 'clemozv3c3eod0bunahh71sx7'
+  req.body.datum = req.body.datum + ":00Z";
+  req.body.herinnering = [req.body.herinnering + ":00Z"];
+  // console.log(req.body)
+  postJson(url2 + "/notities", req.body).then((data) => {
+    // console.log(JSON.stringify(data))
+
+    let newNotitie = { ...req.body };
+    if (data.success) {
+      res.redirect("/agenda");
+      // TODO: squad meegeven, message meegeven
+      // TODO: Toast meegeven aan de homepagina
+    } else {
+      const errormessage = `${data.message}: Mogelijk komt dit door de slug die al bestaat.`;
+      const newdata = { error: errormessage, values: newNotitie };
+
+      res.render("agenda", newdata);
+    }
+  });
+});
+
 // Stel het poortnummer in en start express
-app.set('port', process.env.PORT || 8000)
-app.listen(app.get('port'), function () {
-  console.log(`Application started on http://localhost:${app.get('port')}`)
-})
+app.set("port", process.env.PORT || 8000);
+app.listen(app.get("port"), function () {
+  console.log(`Application started on http://localhost:${app.get("port")}`);
+});
 
 async function fetchJson(url, payload = {}) {
   return await fetch(url, payload)
